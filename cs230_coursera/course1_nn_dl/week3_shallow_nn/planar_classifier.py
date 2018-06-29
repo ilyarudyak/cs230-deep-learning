a49cea23ca1c82dea071351b858dd083729c8245
Ilya Rudyak <ilyarudyak@yahoo.com>
2018-06-29 14:54:52 +0300

    done back prop

--- cs230_coursera/course1_nn_dl/week3_shallow_nn/planar_classifier.py
+++ cs230_coursera/course1_nn_dl/week3_shallow_nn/planar_classifier.py
@@ -1,19 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
-# from testCases import *
+
 import sklearn
 import sklearn.datasets
 import sklearn.linear_model
+
+from course1_nn_dl.week3_shallow_nn.testCases import *
 from course1_nn_dl.week3_shallow_nn.planar_utils import plot_decision_boundary, \
     sigmoid, load_planar_dataset, load_extra_datasets
 
 
-def plot_planar():
+def plot_planar(X, Y):
     plt.scatter(X[0, :], X[1, :], c=Y[0], s=40, cmap=plt.cm.Spectral)
     plt.show()
 
 
-def fit_logistic_regression():
+def fit_logistic_regression(X, Y):
     clf = sklearn.linear_model.LogisticRegressionCV()
     clf.fit(X.T, Y.T)
 
@@ -22,10 +24,128 @@ def fit_logistic_regression():
     plt.show()
 
 
+def layer_sizes(X, Y):
+    """
+    Arguments:
+    X -- input dataset of shape (input size, number of examples)
+    Y -- labels of shape (output size, number of examples)
+
+    Returns:
+    n_x -- the size of the input layer
+    n_h -- the size of the hidden layer
+    n_y -- the size of the output layer
+    """
+    n_x, n_h, n_y = X.shape[0], 4, Y.shape[0]
+    return n_x, n_h, n_y
+
+
+def initialize_parameters(n_x, n_h, n_y):
+    """
+    Argument:
+    n_x -- size of the input layer
+    n_h -- size of the hidden layer
+    n_y -- size of the output layer
+
+    Returns:
+    params -- python dictionary containing your parameters:
+                    W1 -- weight matrix of shape (n_h, n_x)
+                    b1 -- bias vector of shape (n_h, 1)
+                    W2 -- weight matrix of shape (n_y, n_h)
+                    b2 -- bias vector of shape (n_y, 1)
+    """
+    np.random.seed(2)
+    W1, b1 = np.random.randn(n_h, n_x) * .01, np.zeros((n_h, 1))
+    W2, b2 = np.random.randn(n_y, n_h) * .01, np.zeros((n_y, 1))
+    parameters = {"W1": W1, "b1": b1, "W2": W2, "b2": b2}
+    return parameters
+
+
+def forward_propagation(X, parameters):
+    """
+    Argument:
+    X -- input data of size (n_x, m)
+    parameters -- python dictionary containing your parameters (output of initialization function)
+
+    Returns:
+    A2 -- The sigmoid output of the second activation
+    cache -- a dictionary containing "Z1", "A1", "Z2" and "A2"
+    """
+    W1, b1, W2, b2 = parameters['W1'], parameters['b1'], parameters['W2'], parameters['b2']
+
+    Z1 = W1.dot(X) + b1
+    A1 = np.tanh(Z1)
+    Z2 = W2.dot(A1) + b2
+    A2 = sigmoid(Z2)
+    cache = {"Z1": Z1, "A1": A1, "Z2": Z2, "A2": A2}
+    return A2, cache
+
+
+def compute_cost(A2, Y, parameters):
+    """
+    Computes the cross-entropy cost given in equation (13)
+
+    Arguments:
+    A2 -- The sigmoid output of the second activation, of shape (1, number of examples)
+    Y -- "true" labels vector of shape (1, number of examples)
+    parameters -- python dictionary containing your parameters W1, b1, W2 and b2
+
+    Returns:
+    cost -- cross-entropy cost given equation (13)
+    """
+    _, m = Y.shape
+    cost = -(1/m) * np.sum(Y * np.log(A2) + (1 - Y) * np.log(1 - A2))
+    return cost
+
+
+def backward_propagation(parameters, cache, X, Y):
+    """
+    Implement the backward propagation using the instructions above.
+
+    Arguments:
+    parameters -- python dictionary containing our parameters
+    cache -- a dictionary containing "Z1", "A1", "Z2" and "A2".
+    X -- input data of shape (2, number of examples)
+    Y -- "true" labels vector of shape (1, number of examples)
+
+    Returns:
+    grads -- python dictionary containing your gradients with respect to different parameters
+    """
+    _, m = X.shape
+
+    # get params and cache
+    W1, W2 = parameters['W1'], parameters['W2']
+    A1, A2 = cache['A1'], cache['A2']
+
+    # back prop
+    dZ2 = A2 - Y
+    dW2 = (1 / m) * dZ2.dot(A1.T)
+    db2 = (1 / m) * np.sum(dZ2, axis=1, keepdims=True)
+    dZ1 = W2.T.dot(dZ2) * (1-np.power(A1, 2))
+    dW1 = (1 / m) * dZ1.dot(X.T)
+    db1 = (1 / m) * np.sum(dZ1, axis=1, keepdims=True)
+
+    grads = {'dW1': dW1, 'db1': db1, 'dW2': dW2, 'db2': db2}
+    return grads
+
+
+def update_parameters(parameters, grads, learning_rate=1.2):
+    """
+    Updates parameters using the gradient descent update rule given above
+
+    Arguments:
+    parameters -- python dictionary containing your parameters
+    grads -- python dictionary containing your gradients
+
+    Returns:
+    parameters -- python dictionary containing your updated parameters
+    """
+    parameters = None
+    return parameters
+
+
 if __name__ == '__main__':
     np.random.seed(1)
     X, Y = load_planar_dataset()
 
-    # plot_planar()
     fit_logistic_regression()
 
