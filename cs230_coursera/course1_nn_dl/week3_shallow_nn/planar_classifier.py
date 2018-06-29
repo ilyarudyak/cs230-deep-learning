1f0a0096cb1ec399325202e2cba8af55a1be3ca6
Ilya Rudyak <ilyarudyak@yahoo.com>
2018-06-29 15:28:23 +0300

    update parameters

--- cs230_coursera/course1_nn_dl/week3_shallow_nn/planar_classifier.py
+++ cs230_coursera/course1_nn_dl/week3_shallow_nn/planar_classifier.py
@@ -139,7 +139,18 @@ def update_parameters(parameters, grads, learning_rate=1.2):
     Returns:
     parameters -- python dictionary containing your updated parameters
     """
-    parameters = None
+
+    # get params and grads
+    W1, b1, W2, b2 = parameters['W1'], parameters['b1'], parameters['W2'], parameters['b2']
+    dW1, db1, dW2, db2 = grads['dW1'], grads['db1'], grads['dW2'], grads['db2']
+
+    # update params
+    W1 -= learning_rate * dW1
+    b1 -= learning_rate * db1
+    W2 -= learning_rate * dW2
+    b2 -= learning_rate * db2
+
+    parameters = {"W1": W1, "b1": b1, "W2": W2, "b2": b2}
     return parameters
 
 
