c930419d4278aa7903b21abf0c6df9d14f2ff587
Ilya Rudyak <ilyarudyak@yahoo.com>
2018-07-06 18:24:27 +0300

    added notebook

--- learning-tf/ch4_convolutional_nn/mnist/conv_net.py
+++ learning-tf/ch4_convolutional_nn/mnist/conv_net.py
@@ -3,7 +3,6 @@ import numpy as np
 import time
 
 from tensorflow.examples.tutorials.mnist import input_data
-from ch4_convolutional_nn.layers import conv_layer, max_pool_2x2, full_layer
 
 
 class ConvNetMNIST:
@@ -78,6 +77,38 @@ class ConvNetMNIST:
                  for i in range(10)])
 
         print("*** test accuracy: {:.4f} elapsed time: {:.1f}s ***".format(test_accuracy, time.time() - start))
+
+
+def weight_variable(shape):
+    initial = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(initial)
+
+
+def bias_variable(shape):
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial)
+
+
+def conv2d(x, W):
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+
+def max_pool_2x2(x):
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
+                          strides=[1, 2, 2, 1], padding='SAME')
+
+
+def conv_layer(input, shape):
+    W = weight_variable(shape)
+    b = bias_variable([shape[3]])
+    return tf.nn.relu(conv2d(input, W) + b)
+
+
+def full_layer(input, size):
+    in_size = int(input.get_shape()[1])
+    W = weight_variable([in_size, size])
+    b = bias_variable([size])
+    return tf.matmul(input, W) + b
 
 
 if __name__ == '__main__':
