c90c83c650f8fd1bbb013e8b3f85222e399994a8
Ilya Rudyak <ilyarudyak@yahoo.com>
2018-07-06 19:49:06 +0300

    misc changes

--- learning-tf/ch4_convolutional_nn/mnist/conv_net.py
+++ learning-tf/ch4_convolutional_nn/mnist/conv_net.py
@@ -3,6 +3,7 @@ import numpy as np
 import time
 
 from tensorflow.examples.tutorials.mnist import input_data
+from layers import *
 
 
 class ConvNetMNIST:
@@ -14,6 +15,9 @@ class ConvNetMNIST:
 
         self.x = tf.placeholder(tf.float32, shape=[None, 784])
         self.y_ = tf.placeholder(tf.float32, shape=[None, 10])
+
+        # we need a placeholder here in order to change keep_prob
+        # from .5 for training to 1.0 for testing
         self.keep_prob = tf.placeholder(tf.float32)
 
         self.build_conv_net()
@@ -21,6 +25,8 @@ class ConvNetMNIST:
     # noinspection PyAttributeOutsideInit
     def build_conv_net(self):
 
+        # for convolutional NN we reshape back to image shape -
+        # we are going to use spatial structure of the image!
         x_image = tf.reshape(self.x, [-1, 28, 28, 1])
         conv1 = conv_layer(x_image, shape=[5, 5, 1, 32])
         conv1_pool = max_pool_2x2(conv1)
@@ -29,9 +35,9 @@ class ConvNetMNIST:
         conv2_pool = max_pool_2x2(conv2)
 
         conv2_flat = tf.reshape(conv2_pool, [-1, 7 * 7 * 64])
-        full_1 = tf.nn.relu(full_layer(conv2_flat, 1024))
+        full1 = tf.nn.relu(full_layer(conv2_flat, 1024))
 
-        full1_drop = tf.nn.dropout(full_1, keep_prob=self.keep_prob)
+        full1_drop = tf.nn.dropout(full1, keep_prob=self.keep_prob)
 
         y_conv = full_layer(full1_drop, 10)
 
@@ -77,38 +83,6 @@ class ConvNetMNIST:
                  for i in range(10)])
 
         print("*** test accuracy: {:.4f} elapsed time: {:.1f}s ***".format(test_accuracy, time.time() - start))
-
-
-def weight_variable(shape):
-    initial = tf.truncated_normal(shape, stddev=0.1)
-    return tf.Variable(initial)
-
-
-def bias_variable(shape):
-    initial = tf.constant(0.1, shape=shape)
-    return tf.Variable(initial)
-
-
-def conv2d(x, W):
-    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
-
-
-def max_pool_2x2(x):
-    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
-                          strides=[1, 2, 2, 1], padding='SAME')
-
-
-def conv_layer(input, shape):
-    W = weight_variable(shape)
-    b = bias_variable([shape[3]])
-    return tf.nn.relu(conv2d(input, W) + b)
-
-
-def full_layer(input, size):
-    in_size = int(input.get_shape()[1])
-    W = weight_variable([in_size, size])
-    b = bias_variable([size])
-    return tf.matmul(input, W) + b
 
 
 if __name__ == '__main__':
