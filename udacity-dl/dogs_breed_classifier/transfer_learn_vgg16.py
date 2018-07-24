f1fee91607273c731d18c61572dcfb5455dd7042
Ilya Rudyak <ilyarudyak@yahoo.com>
2018-07-24 17:19:56 +0300

    done transfer learning

--- udacity-dl/dogs_breed_classifier/transfer_learn_vgg16.py
+++ udacity-dl/dogs_breed_classifier/transfer_learn_vgg16.py
@@ -17,11 +17,7 @@ def fit_model():
 
     _, train_targets, _, valid_targets, _, _ = get_data()
 
-    VGG16_model = Sequential()
-    VGG16_model.add(GlobalAveragePooling2D(input_shape=train_VGG16.shape[1:]))
-    VGG16_model.add(Dense(133, activation='softmax'))
-
-    VGG16_model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+    VGG16_model = build_model(train_VGG16.shape[1:])
 
     checkpointer = ModelCheckpoint(filepath='saved_models/weights.best.VGG16.hdf5',
                                    verbose=1, save_best_only=True)
@@ -31,5 +27,33 @@ def fit_model():
                     epochs=20, batch_size=20, callbacks=[checkpointer], verbose=2)
 
 
+def build_model(shape):
+    VGG16_model = Sequential()
+    VGG16_model.add(GlobalAveragePooling2D(input_shape=shape))
+    VGG16_model.add(Dense(133, activation='softmax'))
+
+    VGG16_model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+    return VGG16_model
+
+
+def test_model():
+    bottleneck_features = np.load(os.path.join(BOTTLENECK_DIR, 'DogVGG16Data.npz'))
+    train_VGG16 = bottleneck_features['train']
+    test_VGG16 = bottleneck_features['test']
+
+    _, _, _, _, _, test_targets = get_data()
+
+    VGG16_model = build_model(train_VGG16.shape[1:])
+    VGG16_model.load_weights('saved_models/weights.best.VGG16.hdf5')
+    VGG16_predictions = [np.argmax(VGG16_model.predict(np.expand_dims(feature, axis=0)))
+                         for feature in test_VGG16]
+
+    # report test accuracy
+    test_accuracy = 100 * np.sum(np.array(VGG16_predictions) ==
+                                 np.argmax(test_targets, axis=1)) / len(VGG16_predictions)
+    print('test accuracy: {:.1f}%'.format(test_accuracy))
+
+
 if __name__ == '__main__':
-    fit_model()
+    # fit_model()
+    test_model()
