1536601c603ac968601f6a411c1d01a25aad541f
Ilya Rudyak <ilyarudyak@yahoo.com>
2018-07-24 15:18:28 +0300

    add simple transfer learning

--- chollet/conv_nets/transfer_learning.py
+++ chollet/conv_nets/transfer_learning.py
@@ -5,7 +5,68 @@ from keras import models, layers, optimizers
 from keras.applications import VGG16
 from keras.preprocessing.image import ImageDataGenerator
 
+
+def extract_features(directory, sample_count, batch_size=20):
+    datagen = ImageDataGenerator(rescale=1.0 / 255)
+    features = np.zeros(shape=(sample_count, 4, 4, 512))
+    labels = np.zeros(shape=sample_count)
+    generator = datagen.flow_from_directory(
+        directory,
+        target_size=(150, 150),
+        batch_size=batch_size,
+        class_mode='binary')
+    i = 0
+    for inputs_batch, labels_batch in generator:
+        features_batch = conv_base.predict(inputs_batch)
+        features[i * batch_size: (i + 1) * batch_size] = features_batch
+        labels[i * batch_size: (i + 1) * batch_size] = labels_batch
+        i += 1
+        if i * batch_size >= sample_count:
+            # Note that since generators yield data indefinitely in a loop,
+            # we must `break` after every image has been seen once.
+            break
+    return features, labels
+
+
+def fit_transfer_learning_model(epochs=1):
+
+    model = models.Sequential()
+    model.add(layers.Dense(256, activation='relu', input_dim=4 * 4 * 512))
+    model.add(layers.Dropout(0.5))
+    model.add(layers.Dense(1, activation='sigmoid'))
+
+    model.compile(optimizer=optimizers.RMSprop(lr=2e-5),
+                  loss='binary_crossentropy',
+                  metrics=['acc'])
+
+    history = model.fit(train_features, train_labels, verbose=1,
+                        epochs=epochs,
+                        batch_size=20,
+                        validation_data=(validation_features, validation_labels))
+    return history
+
+
+def get_data():
+    base_dir = '/Users/ilyarudyak/data/cats_and_dogs_small'
+    train_dir = os.path.join(base_dir, 'train')
+    validation_dir = os.path.join(base_dir, 'validation')
+    test_dir = os.path.join(base_dir, 'test')
+
+    train_features, train_labels = extract_features(train_dir, 2000)
+    validation_features, validation_labels = extract_features(validation_dir, 1000)
+    test_features, test_labels = extract_features(test_dir, 1000)
+
+    train_features = np.reshape(train_features, (2000, 4 * 4 * 512))
+    validation_features = np.reshape(validation_features, (1000, 4 * 4 * 512))
+    test_features = np.reshape(test_features, (1000, 4 * 4 * 512))
+
+    return train_features, train_labels, validation_features, validation_labels
+
+
 if __name__ == '__main__':
+    np.random.seed(42)
     conv_base = VGG16(weights='imagenet',
                       include_top=False,
                       input_shape=(150, 150, 3))
+    train_features, train_labels, validation_features, validation_labels = get_data()
+    fit_transfer_learning_model(epochs=1)
