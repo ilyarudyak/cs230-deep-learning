0ded6b276ca370fe0b80c9804d8fbf5d6eb441e0
Ilya Rudyak <ilyarudyak@yahoo.com>
2018-07-24 16:20:04 +0300

    precompute weights

--- chollet/conv_nets/transfer_learning.py
+++ chollet/conv_nets/transfer_learning.py
@@ -1,9 +1,13 @@
 import os
 import numpy as np
+import matplotlib.pyplot as plt
 
 from keras import models, layers, optimizers
 from keras.applications import VGG16
 from keras.preprocessing.image import ImageDataGenerator
+
+base_dir = '/Users/ilyarudyak/data/cats_and_dogs_small'
+npzfilename = 'vgg16_precomputed_weights.npz'
 
 
 def extract_features(directory, sample_count, batch_size=20):
@@ -21,33 +25,14 @@ def extract_features(directory, sample_count, batch_size=20):
         features[i * batch_size: (i + 1) * batch_size] = features_batch
         labels[i * batch_size: (i + 1) * batch_size] = labels_batch
         i += 1
-        if i * batch_size >= sample_count:
+        if i * batch_size >= 40:  # sample_count:
             # Note that since generators yield data indefinitely in a loop,
             # we must `break` after every image has been seen once.
             break
     return features, labels
 
 
-def fit_transfer_learning_model(epochs=1):
-
-    model = models.Sequential()
-    model.add(layers.Dense(256, activation='relu', input_dim=4 * 4 * 512))
-    model.add(layers.Dropout(0.5))
-    model.add(layers.Dense(1, activation='sigmoid'))
-
-    model.compile(optimizer=optimizers.RMSprop(lr=2e-5),
-                  loss='binary_crossentropy',
-                  metrics=['acc'])
-
-    history = model.fit(train_features, train_labels, verbose=1,
-                        epochs=epochs,
-                        batch_size=20,
-                        validation_data=(validation_features, validation_labels))
-    return history
-
-
-def get_data():
-    base_dir = '/Users/ilyarudyak/data/cats_and_dogs_small'
+def precompute_data():
     train_dir = os.path.join(base_dir, 'train')
     validation_dir = os.path.join(base_dir, 'validation')
     test_dir = os.path.join(base_dir, 'test')
@@ -60,7 +45,59 @@ def get_data():
     validation_features = np.reshape(validation_features, (1000, 4 * 4 * 512))
     test_features = np.reshape(test_features, (1000, 4 * 4 * 512))
 
-    return train_features, train_labels, validation_features, validation_labels
+    np.savez(os.path.join(base_dir, npzfilename),
+             train_features=train_features, train_labels=train_labels,
+             validation_features=validation_features, validation_labels=validation_labels,
+             test_features=test_features, test_labels=test_labels)
+
+
+def get_precomputed_data():
+    npzfile = np.load(os.path.join(base_dir, npzfilename))
+    return npzfile['train_features'], npzfile['train_labels'], \
+           npzfile['validation_features'], npzfile['validation_labels'], \
+           npzfile['test_features'], npzfile['test_labels']
+
+
+def fit_transfer_learning_model(epochs=1):
+    train_features, train_labels, validation_features, validation_labels, _, _ = get_precomputed_data()
+
+    model = models.Sequential()
+    model.add(layers.Dense(256, activation='relu', input_dim=4 * 4 * 512))
+    model.add(layers.Dropout(0.5))
+    model.add(layers.Dense(1, activation='sigmoid'))
+
+    model.compile(optimizer=optimizers.RMSprop(lr=2e-5),
+                  loss='binary_crossentropy',
+                  metrics=['acc'])
+
+    history = model.fit(train_features, train_labels, verbose=2,
+                        epochs=epochs,
+                        batch_size=20,
+                        validation_data=(validation_features, validation_labels))
+    return history
+
+
+def plot_curves(history):
+    acc = history.history['acc']
+    val_acc = history.history['val_acc']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    epochs = range(len(acc))
+
+    plt.plot(epochs, acc, 'bo', label='Training acc')
+    plt.plot(epochs, val_acc, 'b', label='Validation acc')
+    plt.title('Training and validation accuracy')
+    plt.legend()
+
+    plt.figure()
+
+    plt.plot(epochs, loss, 'bo', label='Training loss')
+    plt.plot(epochs, val_loss, 'b', label='Validation loss')
+    plt.title('Training and validation loss')
+    plt.legend()
+
+    plt.show()
 
 
 if __name__ == '__main__':
@@ -68,5 +105,6 @@ if __name__ == '__main__':
     conv_base = VGG16(weights='imagenet',
                       include_top=False,
                       input_shape=(150, 150, 3))
-    train_features, train_labels, validation_features, validation_labels = get_data()
-    fit_transfer_learning_model(epochs=1)
+    # precompute_data()
+    history = fit_transfer_learning_model(epochs=30)
+    # plot_curves(history)
