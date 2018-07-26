c7dabb505628efbd2a58f593a830eb033f5a1bae
Ilya Rudyak <ilyarudyak@yahoo.com>
2018-07-26 18:37:04 +0300

    done convolution operation

--- cs230_coursera/course4_cnn/week1_intro/step_by_step.py
+++ cs230_coursera/course4_cnn/week1_intro/step_by_step.py
@@ -19,5 +19,85 @@ def zero_pad(X, pad):
     return X_pad
 
 
+def conv_single_step(a_slice_prev, W, b):
+    """
+    Apply one filter defined by parameters W on a single slice (a_slice_prev) of the output activation
+    of the previous layer.
+
+    Arguments:
+    a_slice_prev -- slice of input data of shape (f, f, n_C_prev)
+    W -- Weight parameters contained in a window - matrix of shape (f, f, n_C_prev)
+    b -- Bias parameters contained in a window - matrix of shape (1, 1, 1)
+
+    Returns:
+    Z -- a scalar value, result of convolving the sliding window (W, b) on a slice x of the input data
+    """
+    Z = np.sum(a_slice_prev * W) + b
+    return Z.squeeze()
+
+
+def conv_forward(A_prev, W, b, hparameters):
+    """
+    Implements the forward propagation for a convolution function
+
+    Arguments:
+    A_prev -- output activations of the previous layer, numpy array of shape (m, n_H_prev, n_W_prev, n_C_prev)
+    W -- Weights, numpy array of shape (f, f, n_C_prev, n_C)
+    b -- Biases, numpy array of shape (1, 1, 1, n_C)
+    hparameters -- python dictionary containing "stride" and "pad"
+
+    Returns:
+    Z -- conv output, numpy array of shape (m, n_H, n_W, n_C)
+    cache -- cache of values needed for the conv_backward() function
+    """
+
+    ### START CODE HERE ###
+    # Retrieve dimensions from A_prev's shape (≈1 line)
+    m, n_H_prev, n_W_prev, n_C_prev = A_prev.shape
+
+    # Retrieve dimensions from W's shape (≈1 line)
+    f, f, n_C_prev, n_C = W.shape
+
+    # Retrieve information from "hparameters" (≈2 lines)
+    s, p = hparameters['stride'], hparameters['pad']
+
+    # Compute the dimensions of the CONV output volume using the formula given above.
+    # Hint: use int() to floor. (≈2 lines)
+    n_H = int((n_H_prev + 2 * p - f) / s) + 1
+    n_W = int((n_W_prev + 2 * p - f) / s) + 1
+
+    # Initialize the output volume Z with zeros. (≈1 line)
+    Z = np.zeros((m, n_H, n_W, n_C))
+
+    # Create A_prev_pad by padding A_prev
+    A_prev_pad = zero_pad(A_prev, p)
+
+    for i in range(m):  # loop over the batch of training examples
+        a_prev_pad = A_prev_pad[i]  # Select ith training example's padded activation
+        for h in range(n_H):  # loop over vertical axis of the output volume
+            for w in range(n_W):  # loop over horizontal axis of the output volume
+                for c in range(n_C):  # loop over channels (= #filters) of the output volume
+
+                    # Find the corners of the current "slice" (≈4 lines)
+                    vert_start = h * s
+                    vert_end = vert_start + f
+                    horiz_start = w * s
+                    horiz_end = horiz_start + f
+                    # Use the corners to define the (3D) slice of a_prev_pad (See Hint above the cell). (≈1 line)
+                    a_slice_prev = a_prev_pad[vert_start:vert_end, horiz_start:horiz_end, :]
+                    # Convolve the (3D) slice with the correct filter W and bias b, to get back one output neuron. (≈1 line)
+                    Z[i, h, w, c] = conv_single_step(a_slice_prev, W[:,:,:,c], b[:,:,:,c])
+    ### END CODE HERE ###
+
+    # Making sure your output shape is correct
+    assert (Z.shape == (m, n_H, n_W, n_C))
+
+    # Save information in "cache" for the backprop
+    cache = (A_prev, W, b, hparameters)
+
+    return Z, cache
+
+
 if __name__ == '__main__':
-    pass
+    np.random.seed(1)
+
--- cs230_coursera/course4_cnn/week1_intro/tests/test_step_by_step.py
+++ cs230_coursera/course4_cnn/week1_intro/tests/test_step_by_step.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-from step_by_step import zero_pad
+from step_by_step import zero_pad, conv_single_step, conv_forward
 
 
 def test_zero_pad():
@@ -23,6 +23,34 @@ def test_zero_pad():
                               [0., 0.],
                               [0., 0.]])
     assert np.array_equal(x_pad[1, 1], target_array2)
+
+
+def test_conv_single_step():
+    np.random.seed(1)
+    a_slice_prev = np.random.randn(4, 4, 3)
+    W = np.random.randn(4, 4, 3)
+    b = np.random.randn(1, 1, 1)
+
+    Z = conv_single_step(a_slice_prev, W, b)
+    print(type(Z))
+    assert Z == -6.999089450680221
+
+
+def test_conv_forward():
+    np.random.seed(1)
+    A_prev = np.random.randn(10, 4, 4, 3)
+    W = np.random.randn(2, 2, 3, 8)
+    b = np.random.randn(1, 1, 1, 8)
+    hparameters = {"pad": 2,
+                   "stride": 2}
+
+    Z, cache_conv = conv_forward(A_prev, W, b, hparameters)
+    assert np.isclose(np.mean(Z), 0.0489952035289)
+    assert np.allclose(Z[3, 2, 1], np.array([-0.61490741, -6.7439236,
+                                             -2.55153897, 1.75698377,
+                                             3.56208902, 0.53036437,
+                                             5.18531798, 8.75898442]))
+    assert np.allclose(cache_conv[0][1][2][3], np.array([-0.20075807, 0.18656139, 0.41005165]))
 
 
 if __name__ == '__main__':
