26ad84bed6ea1934bf863790c65ab665745dcac0
Ilya Rudyak <ilyarudyak@yahoo.com>
2018-07-31 13:52:15 +0300

    done with test_yolo_filter_boxes()

--- cs230_coursera/course4_cnn/week3_ob_detect/test_yolo_model.py
+++ cs230_coursera/course4_cnn/week3_ob_detect/test_yolo_model.py
@@ -11,15 +11,19 @@ def test_yolo_filter_boxes():
         box_class_probs = tf.random_normal([19, 19, 5, 80], mean=1, stddev=4, seed=1)
         scores, boxes, classes = yolo_filter_boxes(box_confidence, boxes, box_class_probs, threshold=0.5)
 
-        assert np.isclose(10.7506, scores[2].eval())
+        scores_val = scores.eval()
+        boxes_val = boxes.eval()
+        classes_val = classes.eval()
+
+        assert np.isclose(10.7506, scores_val[2])
 
         target = np.array([8.42653275, 3.27136683, -0.5313437, -4.94137383])
-        assert np.allclose(target, boxes[2].eval())
+        assert np.allclose(target, boxes_val[2])
 
-        assert np.isclose(7, classes[2].eval())
+        assert np.isclose(7, classes_val[2])
 
-        print(f'\n\n---> scores shape: {scores.eval().shape} {scores.eval()[:5]}')
-        print(f'---> classes shape: {classes.eval().shape} {classes.eval()[:5]}')
-        print(f'---> boxes shape: {boxes.eval().shape}\n\n')
+        print(f'\n\n---> scores shape: {scores_val.shape} {scores_val[:5]}')
+        print(f'---> classes shape: {classes_val.shape} {classes_val[:5]}')
+        print(f'---> boxes shape: {boxes_val.shape}\n\n')
 
 
