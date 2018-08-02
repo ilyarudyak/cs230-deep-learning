2c3c59dbe78683943133be7796253dba9a8b880c
Ilya Rudyak <ilyarudyak@yahoo.com>
2018-08-02 18:49:52 +0300

    done face recognition

--- cs230_coursera/course4_cnn/week4_face_rec/face_recognition/fr_model.py
+++ cs230_coursera/course4_cnn/week4_face_rec/face_recognition/fr_model.py
@@ -83,7 +83,6 @@ def verify(image_path, identity, database, model):
     ### START CODE HERE ###
 
     # Step 1: Compute the encoding for the image. Use img_to_encoding() see example above. (≈ 1 line)
-    print('start verification ...')
     encoding = img_to_encoding(image_path, model)
 
     # Step 2: Compute distance with identity's image (≈ 1 line)
@@ -102,14 +101,55 @@ def verify(image_path, identity, database, model):
     return dist, door_open
 
 
+def who_is_it(image_path, database, model):
+    """
+    Implements face recognition for the happy house by finding who is the person on the image_path image.
+
+    Arguments:
+    image_path -- path to an image
+    database -- database containing image encodings along with the name of the person on the image
+    model -- your Inception model instance in Keras
+
+    Returns:
+    min_dist -- the minimum distance between image_path encoding and the encodings from the database
+    identity -- string, the name prediction for the person on image_path
+    """
+
+    ## Step 1: Compute the target "encoding" for the image. Use img_to_encoding() see example above. ## (≈ 1 line)
+    encoding = img_to_encoding(image_path, model)
+
+    ## Step 2: Find the closest encoding ##
+
+    # Initialize "min_dist" to a large value, say 100 (≈1 line)
+    min_dist = 100
+
+    # Loop over the database dictionary's names and encodings.
+    for (name, db_enc) in database.items():
+
+        # Compute L2 distance between the target "encoding" and the current "emb" from the database. (≈ 1 line)
+        dist = np.linalg.norm(encoding - db_enc)
+
+        # If this distance is less than the min_dist, then set min_dist to dist, and identity to name. (≈ 3 lines)
+        if dist < min_dist:
+            min_dist = dist
+            identity = name
+
+    if min_dist > 0.7:
+        print("Not in the database.")
+    else:
+        print("it's " + str(identity) + ", the distance is " + str(min_dist))
+
+    return min_dist, identity
+
+
 if __name__ == '__main__':
     K.set_image_data_format('channels_first')
     # build_model()
 
-    start = time.time()
     FRmodel = load_model_from_disk()
     database = build_database(FRmodel)
-    print(database['dan'].shape)
-    dist, door_open = verify("images/camera_0.jpg", "younes", database, FRmodel)
-    print(dist, door_open)
-    print(f'elapsed:{time.time() - start}')
+
+    # dist, door_open = verify("images/camera_0.jpg", "younes", database, FRmodel)
+    # print(dist, door_open)
+
+    who_is_it("images/camera_0.jpg", database, FRmodel)
