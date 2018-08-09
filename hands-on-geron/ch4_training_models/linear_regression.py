e5335c7b0947194b733a6766af802e7ce37cfecf
Ilya Rudyak <ilyarudyak@yahoo.com>
2018-08-09 13:03:07 +0300

    done with SGD

--- hands-on-geron/ch4_training_models/linear_regression.py
+++ hands-on-geron/ch4_training_models/linear_regression.py
@@ -57,9 +57,56 @@ def batch_grad_descent(n_iterations=1000, lrate=.1, m=N):
     plot_regression(y_pred)
 
 
+def stochastic_grad_descent(epochs=50, m=N):
+    np.random.seed(42)
+    theta = np.random.randn(2, 1)
+    for epoch in range(epochs):
+        for i in range(m):
+
+            # with random index
+            # random_index = np.random.randint(m)
+            # x = X_train_b[random_index, :].reshape(1, 2)
+            # yi = y_train[random_index]
+
+            # with circular index
+            xi = X_train_b[i, :].reshape(1, 2)
+            yi = y_train[i]
+
+            grads = 2 * (xi.dot(theta) - yi) * xi.T
+            lrate = learning_schedule(epoch * m + i)
+            theta -= lrate * grads
+        print(f'epoch:{epoch:2d} rate:{lrate:.4f}')
+
+    print(theta)
+    y_pred = X_test_b.dot(theta)
+    # plot_regression(y_pred)
+
+
+def stochastic_grad_descent2(epochs=50, lrate=.1, m=N):
+    np.random.seed(42)
+    theta = np.random.randn(2, 1)
+    for epoch in range(epochs):
+        for i in range(m):
+            # with circular index
+            xi = X_train_b[i, :].reshape(1, 2)
+            yi = y_train[i]
+
+            grads = 2 * (xi.dot(theta) - yi) * xi.T
+            theta -= lrate * grads
+        lrate *= .90
+        print(f'epoch:{epoch:2d} rate:{lrate:.4f}')
+
+    print(theta)
+
+
+def learning_schedule(t, t0=5, t1=50):
+    return t0 / (t + t1)
+
+
 if __name__ == '__main__':
     X_train, _, X_test, _ = get_data()
     X_train_b, y_train, X_test_b, y_test = preprocess_data()
     # fit_lr_sklearn()
     # normal_equations()
-    batch_grad_descent()
+    # batch_grad_descent()
+    stochastic_grad_descent2()
