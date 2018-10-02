0534970c26f39ca12ada6cc617bb7b3b66f1d1f9
Ilya Rudyak <ilyarudyak@yahoo.com>
2018-10-02 19:56:24 +0300

    done forward prop

--- cs230_coursera/course5_rnn/week1/basic_rnn.py
+++ cs230_coursera/course5_rnn/week1/basic_rnn.py
@@ -96,3 +96,136 @@ def rnn_forward(x, a0, parameters):
     caches = (caches, x)
 
     return a, y_pred, caches
+
+
+def lstm_cell_forward(xt, a_prev, c_prev, parameters):
+    """
+    Implement a single forward step of the LSTM-cell as described in Figure (4)
+
+    Arguments:
+    xt -- your input data at timestep "t", numpy array of shape (n_x, m).
+    a_prev -- Hidden state at timestep "t-1", numpy array of shape (n_a, m)
+    c_prev -- Memory state at timestep "t-1", numpy array of shape (n_a, m)
+    parameters -- python dictionary containing:
+                        Wf -- Weight matrix of the forget gate, numpy array of shape (n_a, n_a + n_x)
+                        bf -- Bias of the forget gate, numpy array of shape (n_a, 1)
+                        Wi -- Weight matrix of the update gate, numpy array of shape (n_a, n_a + n_x)
+                        bi -- Bias of the update gate, numpy array of shape (n_a, 1)
+                        Wc -- Weight matrix of the first "tanh", numpy array of shape (n_a, n_a + n_x)
+                        bc --  Bias of the first "tanh", numpy array of shape (n_a, 1)
+                        Wo -- Weight matrix of the output gate, numpy array of shape (n_a, n_a + n_x)
+                        bo --  Bias of the output gate, numpy array of shape (n_a, 1)
+                        Wy -- Weight matrix relating the hidden-state to the output, numpy array of shape (n_y, n_a)
+                        by -- Bias relating the hidden-state to the output, numpy array of shape (n_y, 1)
+
+    Returns:
+    a_next -- next hidden state, of shape (n_a, m)
+    c_next -- next memory state, of shape (n_a, m)
+    yt_pred -- prediction at timestep "t", numpy array of shape (n_y, m)
+    cache -- tuple of values needed for the backward pass, contains (a_next, c_next, a_prev, c_prev, xt, parameters)
+
+    Note: ft/it/ot stand for the forget/update/output gates, cct stands for the candidate value (c tilde),
+          c stands for the memory value
+    """
+
+    # Retrieve parameters from "parameters"
+    Wf = parameters["Wf"]
+    bf = parameters["bf"]
+    Wi = parameters["Wi"]
+    bi = parameters["bi"]
+    Wc = parameters["Wc"]
+    bc = parameters["bc"]
+    Wo = parameters["Wo"]
+    bo = parameters["bo"]
+    Wy = parameters["Wy"]
+    by = parameters["by"]
+
+    # Retrieve dimensions from shapes of xt and Wy
+    n_x, m = xt.shape
+    n_y, n_a = Wy.shape
+
+    ### START CODE HERE ###
+    # Concatenate a_prev and xt (≈3 lines)
+    concat = np.zeros((n_a + n_x, m))
+    concat[0:n_a, :] = a_prev
+    concat[n_a:, :] = xt
+
+    # Compute values for ft, it, cct, c_next, ot, a_next using the formulas given figure (4) (≈6 lines)
+    ft = sigmoid(Wf.dot(concat) + bf)  # forget gate
+    it = sigmoid(Wi.dot(concat) + bi)  # update gate
+    cct = np.tanh(Wc.dot(concat) + bc)  # candidate value
+    c_next = ft * c_prev + it * cct  # actual c
+    ot = sigmoid(Wo.dot(concat) + bo)  # output gate
+    a_next = ot * np.tanh(c_next)
+
+    # Compute prediction of the LSTM cell (≈1 line)
+    yt_pred = softmax(Wy.dot(a_next) + by)
+    ### END CODE HERE ###
+
+    # store values needed for backward propagation in cache
+    cache = (a_next, c_next, a_prev, c_prev, ft, it, cct, ot, xt, parameters)
+
+    return a_next, c_next, yt_pred, cache
+
+
+def lstm_forward(x, a0, parameters):
+    """
+    Implement the forward propagation of the recurrent neural network using an LSTM-cell described in Figure (3).
+
+    Arguments:
+    x -- Input data for every time-step, of shape (n_x, m, T_x).
+    a0 -- Initial hidden state, of shape (n_a, m)
+    parameters -- python dictionary containing:
+                        Wf -- Weight matrix of the forget gate, numpy array of shape (n_a, n_a + n_x)
+                        bf -- Bias of the forget gate, numpy array of shape (n_a, 1)
+                        Wi -- Weight matrix of the update gate, numpy array of shape (n_a, n_a + n_x)
+                        bi -- Bias of the update gate, numpy array of shape (n_a, 1)
+                        Wc -- Weight matrix of the first "tanh", numpy array of shape (n_a, n_a + n_x)
+                        bc -- Bias of the first "tanh", numpy array of shape (n_a, 1)
+                        Wo -- Weight matrix of the output gate, numpy array of shape (n_a, n_a + n_x)
+                        bo -- Bias of the output gate, numpy array of shape (n_a, 1)
+                        Wy -- Weight matrix relating the hidden-state to the output, numpy array of shape (n_y, n_a)
+                        by -- Bias relating the hidden-state to the output, numpy array of shape (n_y, 1)
+
+    Returns:
+    a -- Hidden states for every time-step, numpy array of shape (n_a, m, T_x)
+    y -- Predictions for every time-step, numpy array of shape (n_y, m, T_x)
+    caches -- tuple of values needed for the backward pass, contains (list of all the caches, x)
+    """
+
+    # Initialize "caches", which will track the list of all the caches
+    caches = []
+
+    ### START CODE HERE ###
+    # Retrieve dimensions from shapes of x and Wy (≈2 lines)
+    n_x, m, T_x = x.shape
+    n_y, n_a = parameters["Wy"].shape
+
+    # initialize "a", "c" and "y" with zeros (≈3 lines)
+    a = np.zeros((n_a, m, T_x))
+    c = a
+    y = np.zeros((n_y, m, T_x))
+
+    # Initialize a_next and c_next (≈2 lines)
+    a_next = a0
+    c_next = np.zeros(a_next.shape)
+
+    # loop over all time-steps
+    for t in range(T_x):
+        # Update next hidden state, next memory state, compute the prediction, get the cache (≈1 line)
+        a_next, c_next, yt, cache = lstm_cell_forward(x[:, :, t], a_next, c_next, parameters)
+        # Save the value of the new "next" hidden state in a (≈1 line)
+        a[:, :, t] = a_next
+        # Save the value of the prediction in y (≈1 line)
+        y[:, :, t] = yt
+        # Save the value of the next cell state (≈1 line)
+        c[:, :, t] = c_next
+        # Append the cache into caches (≈1 line)
+        caches.append(cache)
+
+    ### END CODE HERE ###
+
+    # store values needed for backward propagation in cache
+    caches = (caches, x)
+
+    return a, y, c, caches
