6ff170c664ccb297849d35a4550d4199d13de4c1
Ilya Rudyak <ilyarudyak@yahoo.com>
2018-10-05 14:05:58 +0300

    add clipping

--- cs230_coursera/course5_rnn/week1/dinosaur_island_v2/blocks.py
+++ cs230_coursera/course5_rnn/week1/dinosaur_island_v2/blocks.py
@@ -6,3 +6,11 @@ def softmax(x):
     return ex / np.sum(x, axis=0)
 
 
+def clip(gradients, max_value):
+    for grad_value in gradients.values():
+        # in-place clipping
+        np.clip(grad_value, -max_value, max_value, out=grad_value)
+
+
+def sample(parameters, char_to_ix, seed):
+    pass
