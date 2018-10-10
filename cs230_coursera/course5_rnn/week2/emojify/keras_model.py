458d3fca7bf17ee14fd40ddb1438c2fa65167b1c
Ilya Rudyak <ilyarudyak@yahoo.com>
2018-10-10 23:39:59 +0300

    add embedding layer

--- cs230_coursera/course5_rnn/week2/emojify/keras_model.py
+++ cs230_coursera/course5_rnn/week2/emojify/keras_model.py
@@ -1,4 +1,5 @@
 import numpy as np
+
 np.random.seed(0)
 from keras.models import Model
 from keras.layers import Dense, Input, Dropout, LSTM, Activation
@@ -7,8 +8,8 @@ from keras.preprocessing import sequence
 from keras.initializers import glorot_uniform
 np.random.seed(1)
 
-
 from emo_utils import read_csv, convert_to_one_hot, read_glove_vecs
+
 
 def get_data():
     X_train, Y_train = read_csv('data/train_emoji.csv')
@@ -19,8 +20,6 @@ def get_data():
     word_to_index, index_to_word, word_to_vec_map = \
         read_glove_vecs('../word_vectors/data/glove.6B.50d.txt')
     return word_to_index, index_to_word, word_to_vec_map
-
-
 
 
 def sentences_to_indices(X, word_to_index, max_len):
@@ -52,10 +51,46 @@ def get_indices(sentence, word_to_index):
     return len(words), np.array([word_to_index[word] for word in words])
 
 
+def pretrained_embedding_layer(word_to_vec_map, word_to_index):
+    """
+    Creates a Keras Embedding() layer and loads in pre-trained GloVe 50-dimensional vectors.
+
+    Arguments:
+    word_to_vec_map -- dictionary mapping words to their GloVe vector representation.
+    word_to_index -- dictionary mapping from words to their indices in the vocabulary (400,001 words)
+
+    Returns:
+    embedding_layer -- pretrained layer Keras instance
+    """
+
+    vocab_len = len(word_to_index) + 1
+    emb_dim = 50
+
+    # Initialize the embedding matrix as a numpy array of zeros of shape
+    # (vocab_len, dimensions of word vectors = emb_dim)
+    emb_matrix = np.zeros((vocab_len, emb_dim))
+
+    # Set each row "index" of the embedding matrix to be the word vector
+    # representation of the "index"th word of the vocabulary
+    for word, index in word_to_index.items():
+        emb_matrix[index, :] = word_to_vec_map[word]
+
+    # Define Keras embedding layer with the correct output/input sizes, make it non-trainable.
+    # Use Embedding(...). Make sure to set trainable=False.
+    embedding_layer = Embedding(vocab_len, emb_dim, trainable=False)
+    embedding_layer.build((None,))
+    embedding_layer.set_weights([emb_matrix])
+
+    return embedding_layer
+
+
 if __name__ == '__main__':
-    word_to_index, _, _ = get_data()
+    word_to_index, index_to_word, word_to_vec_map = get_data()
 
     # X1 = np.array(["funny lol", "lets play baseball", "food is ready for you"])
     # X1_indices = sentences_to_indices(X1, word_to_index, max_len=5)
     # print("X1 =", X1)
     # print("X1_indices =", X1_indices)
+
+    embedding_layer = pretrained_embedding_layer(word_to_vec_map, word_to_index)
+    print("weights[0][1][3] =", embedding_layer.get_weights()[0][1][3])
