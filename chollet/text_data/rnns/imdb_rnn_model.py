b3158af4adbdee6ad89d252c7b202e13f587174c
Ilya Rudyak <ilyarudyak@yahoo.com>
2018-10-24 17:07:36 +0300

    done SimpleRNN model in keras

---
+++ chollet/text_data/rnns/imdb_rnn_model.py
@@ -0,0 +1,36 @@
+from keras.datasets import imdb
+from keras.preprocessing import sequence
+from keras.layers import Embedding, SimpleRNN, Dense
+from keras.models import Sequential
+
+
+def get_data():
+
+    (input_train, y_train), (input_test, y_test) = imdb.load_data(num_words=max_features)
+
+    input_train = sequence.pad_sequences(input_train, maxlen=maxlen)
+    input_test = sequence.pad_sequences(input_test, maxlen=maxlen)
+
+    return input_train, y_train, input_test, y_test
+
+
+def imdb_model(epochs=10):
+    model = Sequential()
+    model.add(Embedding(max_features, 32))
+    model.add(SimpleRNN(32))
+    model.add(Dense(1, activation='sigmoid'))
+
+    model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
+    history = model.fit(input_train, y_train,
+                        epochs=epochs,
+                        batch_size=128,
+                        validation_split=0.2)
+
+
+if __name__ == '__main__':
+    max_features = 10000  # number of words to consider as features
+    maxlen = 500  # cut texts after this number of words (among top max_features most common words)
+    batch_size = 32
+
+    input_train, y_train, input_test, y_test = get_data()
+    imdb_model()
