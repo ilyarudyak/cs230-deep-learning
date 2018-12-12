2e572711b8c18471d085c9685ee295d0392efb5d
Ilya Rudyak <ilyarudyak@yahoo.com>
2018-12-12 11:12:08 +0300

    done batch sizes search

--- chollet/mnist_kaggle/hp_search.py
+++ chollet/mnist_kaggle/hp_search.py
@@ -6,21 +6,20 @@ class HyperParamSearcher:
     def __init__(self, models):
         self.models = models
 
-    def batch_size_search(self):
+    def batch_size_search(self, filename):
         val_accs = {}
-        val_accs_max = {}
         for model in self.models:
             hist: History = model.train_model_aug()
             val_acc = hist.history['val_acc']
             val_accs[model.batch_size] = val_acc
-            val_accs_max[model.batch_size] = max(val_acc)
-        return val_accs, val_accs_max
-            
+
+        df = pd.DataFrame(val_accs)
+        df.to_csv(filename, index=False)
+
 
 if __name__ == '__main__':
     batch_sizes = [64, 128, 256, 512]
-    models = [MnistModel(name='krohn', epochs=1, batch_size=bs, verbose=0) for bs in batch_sizes]
+    models = [MnistModel(name='krohn', epochs=50, batch_size=bs) for bs in batch_sizes]
 
     hps = HyperParamSearcher(models)
-    val_accs, val_accs_max = hps.batch_size_search()
-    print(val_accs, val_accs_max)
+    hps.batch_size_search('hp/hp_batch_size.csv')
